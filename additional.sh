d6dec1106b9261b517e3264403ada8aee95da386
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-26 15:52:29 -0500

    additional.sh

--- additional.sh
+++ additional.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-count=$(wc -l $(PWD)/tokens_seed.txt)
+#count=$(wc -l $PWD/tokens_seed.txt | awk '{print substr($0,1,2)}')
 export count
 #echo $count
+# count=5 && echo `expr 12 - $count`
 gawk '
-BEGIN   { srand(); c = $(eval 12 - $count) }
+BEGIN   { srand(); c = 8 }
 c/NR >= rand() { lines[x++ % c] = $0 }
 END { for (i in lines)  print lines[i] }
 
